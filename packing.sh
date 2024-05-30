691179dd152228e627dff118547c73764ae51bed
Eiji Hachiya <hachiyae@gmail.com>
2024-05-31 02:10:22 +0900

    feat: add version and clean working directory

--- packing.sh
+++ packing.sh
@@ -4,6 +4,7 @@ set -eu
 
 DIST_DIR='free-kintai-chrome-assistant'
 FILE_NAME='free-kintai-chrome-assistant'
+VERSION=$(jq -r '.version' manifest.json)
 
 [ ! -d "${DIST_DIR}" ] && mkdir ${DIST_DIR}
 rm -rf ${DIST_DIR:?}/*
@@ -14,4 +15,6 @@ cp -p modify_headers.json ${DIST_DIR}/
 cp -pr node_modules ${DIST_DIR}/
 cp -pr src ${DIST_DIR}/
 cp -pr icons ${DIST_DIR}/
-zip -r ${FILE_NAME}.zip ${DIST_DIR}
+zip -r "${FILE_NAME}-${VERSION}".zip ${DIST_DIR}
+
+rm -rf ${DIST_DIR:?}
